aaad9e0006ce6620c6a4448b35a19e563207ee0c
Kimon Meier <kimon.meier@gmail.com>
2022-01-12 21:08:10 +0100

    Created a basic updater class.
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

---
+++ Daemon.Updater/Updater.cs
@@ -0,0 +1,21 @@
+﻿namespace Daemon.Updater;
+
+public class Updater
+{
+    private const string UPDATER_URL = "";
+    
+    public Updater()
+    {
+        
+    }
+
+    public bool HasUpdates()
+    {
+        return false;
+    }
+
+    public bool ExecuteUpdate()
+    {
+        return true;
+    }
+}
