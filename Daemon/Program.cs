7b138c62e0618702004e5385bac02c72fac13fc3
Kimon Meier <kimon.meier@gmail.com>
2022-01-12 21:08:44 +0100

    Implemented the logic to start the Program with the possibility to update the other modules before the start
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

---
+++ Daemon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace Daemon;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var updater = new Updater.Updater();
+
+        if (updater.HasUpdates())
+        {
+            if (!updater.ExecuteUpdate())
+            {
+                
+                return;
+            }
+            
+            
+        }
+        
+        Console.WriteLine("No Updates");
+
+        Assembly assembly = Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "Daemon.Core.dll"));
+        Type mainClass = assembly.GetType("Daemon.Core.MainApp") ?? throw new InvalidOperationException();
+        dynamic? mainApp = Activator.CreateInstance(mainClass);
+
+        if (mainApp == null)
+        {
+            return;
+        }
+
+        mainApp.StartApp();
+    }
+}
