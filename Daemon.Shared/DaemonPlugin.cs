4af783f2e8222d678bf0b39030d08ab28a58d4d5
Kimon Meier <kimon.meier@gmail.com>
2022-01-17 22:15:18 +0100

    Added a shared project for the plugins
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

---
+++ Daemon.Shared/DaemonPlugin.cs
@@ -0,0 +1,53 @@
+﻿using log4net;
+
+namespace Daemon.Shared;
+
+/// <summary>
+/// This is the class which is getting called by Core upon loading the Plugin
+/// </summary>
+public abstract class DaemonPlugin {
+	#region Instanzvariablen
+
+	private ILog? sysLog;
+
+	#endregion
+
+	#region Protected Properties
+
+	#region protected ILog Logger
+
+	/// <summary>
+	/// Gets the Logger of the specified Plugin
+	/// </summary>
+	protected ILog Logger {
+		get {
+			return this.sysLog ??= LogManager.GetLogger(this.GetType().FullName);
+		}
+	}
+
+	#endregion
+
+	#endregion
+
+	#region Public Methods
+
+	#region public abstract void OnPluginLoad()
+
+	/// <summary>
+	/// This Method is getting called on loading the plugin
+	/// </summary>
+	public abstract void OnPluginLoad();
+
+	#endregion
+
+	#region public abstract void OnPluginDisable()
+
+	/// <summary>
+	/// This Method is getting called on unloading the plugin
+	/// </summary>
+	public abstract void OnPluginDisable();
+
+	#endregion
+
+	#endregion
+}
