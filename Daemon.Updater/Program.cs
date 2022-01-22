2bd0f63993c327ae23a12d07f536aa46e1073260
Kimon Meier <kimon.meier@gmail.com>
2022-01-22 03:27:50 +0100

    Added a new Updater Project for the new plan on how to handle the whole update
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

---
+++ Daemon.Updater/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+
+public class Program {
+	private readonly Logger logger;
+
+	public static void Main(string[] args) {
+		//TODO: Starts the Updater and guides the user through the process
+	}
+
+	private void searchForUpdate() {
+		//TODO: Search for the Update
+	}
+
+	private void doUpdate() {
+		//TODO: Do the update
+	}
+
+	private void isUpdateCompatabile() {
+		//TODO: Notify User that he has to do the update manually
+	}
+}
