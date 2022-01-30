10f6c5c73624766a3689b50c732c2e33f9947234
Kimon Meier <kimon.meier@gmail.com>
2022-01-30 22:56:56 +0100

    Added the base class to the plugin and inserted the IContainer for the dependency injection
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

--- Daemon.Shared/Plugins/DaemonPlugin.cs
+++ Daemon.Shared/Plugins/DaemonPlugin.cs
@@ -1,28 +1,19 @@
-﻿namespace Daemon.Shared.Plugins;
+﻿using Autofac;
+using Daemon.Shared.Basic;
+
+namespace Daemon.Shared.Plugins;
 
 /// <summary>
 /// This is the class which is getting called by Core upon loading the Plugin
 /// </summary>
-public abstract class DaemonPlugin {
-	#region Public Methods
-
-	#region public abstract void OnPluginLoad()
-
+public abstract class DaemonPlugin : BaseClass {
 	/// <summary>
 	/// This Method is getting called on loading the plugin
 	/// </summary>
-	public abstract void OnPluginLoad();
-
-	#endregion
-
-	#region public abstract void OnPluginDisable()
+	public abstract void OnPluginLoad(IContainer container);
 
 	/// <summary>
 	/// This Method is getting called on unloading the plugin
 	/// </summary>
 	public abstract void OnPluginDisable();
-
-	#endregion
-
-	#endregion
 }
