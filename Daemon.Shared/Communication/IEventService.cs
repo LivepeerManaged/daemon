3d4bc481fdd7b6d8e35e7c751d8c080ce062d66d
Kimon Meier <kimon.meier@gmail.com>
2022-01-31 00:32:05 +0100

    Removed usless namespace
    Signed-off-by: Kimon Meier <kimon.meier@gmail.com>

--- Daemon.Shared/Communication/IEventService.cs
+++ Daemon.Shared/Communication/IEventService.cs
@@ -1,7 +1,7 @@
 ﻿namespace Daemon.Shared.Communication;
 
 public interface IEventService {
-	void OnEvent<T>(Action<Event> onCall) where T : Daemon.Shared.Communication.Event;
+	void OnEvent<T>(Action<Event> onCall) where T : Event;
 
 	void TriggerEvent<T>(Event @event);
 }
